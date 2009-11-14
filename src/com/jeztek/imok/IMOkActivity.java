82b13edc4a74496ecfe785dd0c21b2a759425918
Eric Park <epark@yowie.local>
2009-11-13 17:15:22 -0800

    initial commit

---
+++ src/com/jeztek/imok/IMOkActivity.java
@@ -0,0 +1,13 @@
+package com.jeztek.imok;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IMOkActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
