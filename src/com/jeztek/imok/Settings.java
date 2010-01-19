03fe946f344948accb96e31396582183a3077420
Ted Morse <tmorse@gmail.com>
2010-01-18 21:50:03 -0800

    Added "verify" preference to send an SMS to verify the phone number with the gateway.

--- src/com/jeztek/imok/Settings.java
+++ src/com/jeztek/imok/Settings.java
@@ -9,6 +9,7 @@ public final class Settings {
 	public static final String URL_REPORT = "/data/imok/";
 
 	public static final String GATEWAY_PHONE = "settings_gateway_phone";
+	public static final String GATEWAY_VERIFY = "settings_gateway_verified";
 	public static final String USER_KEY = "settings_user_key";
 	public static final String RESET = "settings_reset";
 	public static final String DEFAULT_USER_KEY = "00000";
--- src/com/jeztek/imok/SettingsActivity.java
+++ src/com/jeztek/imok/SettingsActivity.java
@@ -1,15 +1,39 @@
 package com.jeztek.imok;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.widget.Toast;
 
-public class SettingsActivity extends PreferenceActivity implements OnPreferenceChangeListener {
+public class SettingsActivity extends PreferenceActivity 
+	implements OnPreferenceChangeListener, OnPreferenceClickListener {
+	
+	public static final String TAG = "ImOk.SettingsActivity";
+	private static final String BROADCAST_SMS = "com.jeztek.imok.settings.BROADCAST_SMS";
+		
+	private static final int DIALOG_ENSURE = 1;
+	private static final int DIALOG_INSTRUCT = 2;
+	private static final int DIALOG_ERROR = 3;
+	
+	private Preference mVerifyPreference;
+	private boolean mVerified = false;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,6 +49,33 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		// Update summary automatically
 		gatewayPhone.setOnPreferenceChangeListener(this);
 		gatewayPhone.setSummary(gatewayPhone.getText());
+		
+		mVerifyPreference = findPreference(Settings.GATEWAY_VERIFY);
+		mVerifyPreference.setOnPreferenceClickListener(this);
+		mVerified = mVerifyPreference.getSharedPreferences().getBoolean(Settings.GATEWAY_VERIFY, mVerified);
+	}
+	
+	@Override
+	protected void onPause() {
+		mVerifyPreference
+			.getEditor().putBoolean(Settings.GATEWAY_VERIFY, mVerified)
+			.commit();
+		
+		if (mReceiverRegistered)
+			unregisterReceiver(mSmsSent);
+		
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		if (mVerified == true) {
+			mVerifyPreference.setSummary(getText(R.string.settings_gateway_verified));
+		} else {
+			mVerifyPreference.setSummary(getText(R.string.settings_gateway_unverified));
+		}
 	}
 
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -36,65 +87,123 @@ public class SettingsActivity extends PreferenceActivity implements OnPreference
 		
 		return true;
 	}
-}
-
-/*
-public class SettingsActivity extends PreferenceActivity implements
-		OnPreferenceChangeListener, OnPreferenceClickListener {
-
-	private EditTextPreference mUserKeyPref;
+	
+	public boolean onPreferenceClick(Preference preference) {
+		showDialog(DIALOG_ENSURE);
+		return true;
+	}
+	
+	private static final int ENSURE_MESSAGE_CANCEL = 1;
+	private static final int ENSURE_MESSAGE_VERIFY = 2;
+	private static final int ENSURE_MESSAGE_VERIFIED = 3;
+	
+	private final Handler mEnsureHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case ENSURE_MESSAGE_VERIFY:
+				verify();
+				break;
+				
+			case ENSURE_MESSAGE_VERIFIED:
+				mVerified = true;
+				mVerifyPreference.getSharedPreferences().edit().putBoolean(Settings.GATEWAY_VERIFY, mVerified).commit();
+				mVerifyPreference.setSummary(getText(R.string.settings_gateway_verified));
+				break;
+			}
+		}
+	};
+	
+	private final Message mVerifyMessage = new Message();
+	private final Message mVerifiedMessage = new Message();
+	private final Message mCancelMessage = new Message();
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected Dialog onCreateDialog(int id) {
+		mVerifyMessage.what = ENSURE_MESSAGE_VERIFY;
+		mVerifyMessage.setTarget(mEnsureHandler);
 		
-		setPreferenceScreen();
-	}
-
-	private PreferenceScreen createSettingsHierarchy() {
-		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-		String userKey = settings.getString(Settings.USER_KEY, "0");
-
-		// Root
-		PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
+		mVerifiedMessage.what = ENSURE_MESSAGE_VERIFIED;
+		mVerifiedMessage.setTarget(mEnsureHandler);
 		
-        // User settings
-        PreferenceCategory userCategory = new PreferenceCategory(this);
-        userCategory.setTitle(R.string.settings_category_user);
-        root.addPreference(userCategory);
-
-        // User key
-        mUserKeyPref = new EditTextPreference(this);
-        mUserKeyPref.setKey(Settings.USER_KEY);
-        mUserKeyPref.setDialogTitle(R.string.settings_user_key_title);
-        mUserKeyPref.setTitle(R.string.settings_user_key_title);
-        mUserKeyPref.setSummary(userKey);
-        mUserKeyPref.setOnPreferenceChangeListener(this);
-        userCategory.addPreference(mUserKeyPref);
-
-        // Reset settings
-        Preference resetPref = new Preference(this);
-        resetPref.setKey(Settings.RESET);
-        resetPref.setTitle(R.string.settings_reset_title);
-        resetPref.setOnPreferenceClickListener(this);
-        userCategory.addPreference(resetPref);
-        
-		return root;
+		mCancelMessage.what = ENSURE_MESSAGE_CANCEL;
+		mCancelMessage.setTarget(mEnsureHandler);		
+		
+		switch (id) {
+		case DIALOG_ENSURE:
+			AlertDialog.Builder ensureBuilder = new AlertDialog.Builder(this);
+			AlertDialog ensureDialog = ensureBuilder
+				.setMessage(R.string.settings_ensure_message_unverified)
+				.create();
+			ensureDialog.setButton(Dialog.BUTTON_POSITIVE, "Yes", mVerifyMessage);
+			ensureDialog.setButton(Dialog.BUTTON_NEGATIVE, "No", mCancelMessage);
+			ensureDialog.setCancelMessage(mCancelMessage);
+			ensureDialog.setDismissMessage(mCancelMessage);			
+			return ensureDialog;
+			
+		case DIALOG_INSTRUCT:
+			AlertDialog.Builder instructBuilder = new AlertDialog.Builder(this);
+			AlertDialog instructDialog = instructBuilder
+				.setTitle(R.string.settings_dialog_instruct_title)
+				.setMessage(R.string.settings_dialog_instruct_message)
+				.create();
+			instructDialog.setButton(Dialog.BUTTON_NEUTRAL, "Ok", mVerifiedMessage);
+			return instructDialog;
+		case DIALOG_ERROR:
+			AlertDialog.Builder errorBuilder = new AlertDialog.Builder(this);
+			return errorBuilder
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setTitle(R.string.settings_dialog_error_title)
+				.setMessage(R.string.settings_dialog_error_message)
+				.setNeutralButton("Ok", null)
+				.create();
+		}
+		
+		return super.onCreateDialog(id);
 	}
-	public boolean onPreferenceChange(Preference preference, Object newValue) {
-		preference.setSummary((String)newValue);
-		return true;
+
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		switch (id) {
+		case DIALOG_ENSURE:
+			AlertDialog alertDialog = (AlertDialog) dialog;
+			if (mVerified) {
+				alertDialog.setMessage(getText(R.string.settings_ensure_message_verified));
+			} else {
+				alertDialog.setMessage(getText(R.string.settings_ensure_message_unverified));
+			}
+			return;
+		}
+		super.onPrepareDialog(id, dialog);
 	}
+	
+	private boolean mReceiverRegistered = false;
+	private BroadcastReceiver mSmsSent = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if (this.getResultCode() == Activity.RESULT_OK) {
+				showDialog(DIALOG_INSTRUCT);
+			} else {
+				showDialog(DIALOG_ERROR);
+			}
+			
+			SettingsActivity.this.unregisterReceiver(mSmsSent);
+			mReceiverRegistered = false;
+		}
+	};
 
-	public boolean onPreferenceClick(Preference preference) {
-        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString(Settings.USER_KEY, "0");
-        editor.commit();            
-
-        mUserKeyPref.setSummary("0");
-        
-		return true;
+	private void verify() {
+		Log.d(TAG, "Sending verification SMS");
+		
+		registerReceiver(mSmsSent, new IntentFilter(BROADCAST_SMS));
+		mReceiverRegistered = true;
+		
+		String number = 
+			mVerifyPreference.getSharedPreferences().getString(Settings.GATEWAY_PHONE, "650-691-5005");
+		
+		SmsManager smsManager = SmsManager.getDefault();
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(BROADCAST_SMS), 0);
+		smsManager.sendTextMessage(number, null, "VERIFY", pendingIntent, null);
+		
+		Toast.makeText(this, "SMS Sent", Toast.LENGTH_SHORT).show();
 	}
 }
-*/
